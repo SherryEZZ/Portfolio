97e9a37d02d6a91b27e9ed14211268a480bc0aa2
Sherry Ezz <121711142+SherryEZZ@users.noreply.github.com>
2024-03-22 20:06:18 +0200

    Add files via upload

---
+++ action.js
@@ -0,0 +1,13 @@
+function viewLogIn() {
+    document.getElementById('log').style.display = 'block'
+}
+function closeLogIn() {
+    document.getElementById('log').style.display = 'none'
+}
+function openNav() {
+    document.getElementById("Sidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("Sidenav").style.width = "0";
+}
